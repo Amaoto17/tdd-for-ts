475ece0a8234d109beae9819b13e061a24ed9df7
amaoto <amaoto.n17@gmail.com>
2019-10-22 21:46:15 +0900

    Finished chapter6

--- __tests__/money.spec.ts
+++ __tests__/money.spec.ts
@@ -9,10 +9,12 @@ test('multiplication', () => {
 test('equality', () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
   expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+  expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
+  expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
 });
 
 test('franc multiplication', () => {
   let five = new Franc(5);
   expect(five.times(2)).toStrictEqual(new Franc(10));
   expect(five.times(3)).toStrictEqual(new Franc(15));
-})
+});
--- src/money.ts
+++ src/money.ts
@@ -1,25 +1,24 @@
-export class Dollar {
-  constructor(private amount: number) {}
+class Money {
+  constructor(protected amount: number) {}
+
+  equals(object: any): boolean {
+    let money = object as Money;
+    return this.amount == money.amount;
+  }
+}
+
+export class Dollar extends Money {
+  constructor(amount: number) { super(amount); }
 
   times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
   }
-
-  equals(object: any): boolean {
-    let dollar = object as Dollar;
-    return this.amount == dollar.amount;
-  }
 }
 
-export class Franc {
-  constructor(private amount: number) {}
+export class Franc extends Money {
+  constructor(amount: number) { super(amount); }
 
   times(multiplier: number): Franc {
     return new Franc(this.amount * multiplier);
-  }
-
-  equals(object: any): boolean {
-    let franc = object as Franc;
-    return this.amount == franc.amount;
   }
 }
