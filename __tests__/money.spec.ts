2cab02fb30708224434f04ddba417095ad73c3b5
amaoto <amaoto.n17@gmail.com>
2019-10-22 21:34:41 +0900

    Finished chapter5

--- __tests__/money.spec.ts
+++ __tests__/money.spec.ts
@@ -1,4 +1,4 @@
-import { Dollar } from '../src/money';
+import { Dollar, Franc } from '../src/money';
 
 test('multiplication', () => {
   let five = new Dollar(5);
@@ -10,3 +10,9 @@ test('equality', () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
   expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
 });
+
+test('franc multiplication', () => {
+  let five = new Franc(5);
+  expect(five.times(2)).toStrictEqual(new Franc(10));
+  expect(five.times(3)).toStrictEqual(new Franc(15));
+})
--- src/money.ts
+++ src/money.ts
@@ -10,3 +10,16 @@ export class Dollar {
     return this.amount == dollar.amount;
   }
 }
+
+export class Franc {
+  constructor(private amount: number) {}
+
+  times(multiplier: number): Franc {
+    return new Franc(this.amount * multiplier);
+  }
+
+  equals(object: any): boolean {
+    let franc = object as Franc;
+    return this.amount == franc.amount;
+  }
+}
